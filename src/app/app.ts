cd0dd88a2c08aff3f323298717074f7cda0a44b5
MohamedAgiba <mohamed.agiba@cic.ae>
2026-08-09 18:05:41 +0300

    feat(app): add payment amount input and display
    include CurrencyPipe for currency formatting, add amount state variable, and add input handler to convert input value to a valid number

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
-import { TitleCasePipe, DatePipe } from '@angular/common';
+import { TitleCasePipe, DatePipe, CurrencyPipe } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [TitleCasePipe, DatePipe],
+  imports: [TitleCasePipe, DatePipe, CurrencyPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   name: string = '';
   date: string = '';
+  amount: number = 0;
 
   onNameChange(value: string) {
     this.name = value;
   }
+
   onDateChange(value: string) {
     this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = Number(value) || 0;
   }
 }
